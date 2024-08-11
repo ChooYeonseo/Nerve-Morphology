23983152129008255654db2083902d5fafd958ff
INERTIAsci <INERTIASCI@gmail.com>
2024-08-11 18:43:47 +0900

    cleaned

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-bg_img = mpimg.imread(f"./data/left.png")
-fig, ax = plt.subplots()
-ax.imshow(bg_img, extent=[-3, 10, -4, 10], aspect='auto')
-plt.show()
